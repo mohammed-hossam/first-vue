1c625ecafcfee6644d2c284a6501a255fcc1f851
mohammed-hossam <mohammedhossam7777@gmail.com>
2021-10-05 02:07:35 +0200

    first notes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,22 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <p>{{ fullName }}</p>
+    <!-- <input type="number" v-model="number1" /> -->
+    <!-- or -->
+    <input type="number" v-bind:value="number1" @input="setStateNumber1" />
+    <input type="number" v-model="number2" />
+
+    <button type="button" v-on:click="add">Add</button>
+    <br />
+
+    <button type="button" v-on:click="welcome('hy how are you', $event)">
+      log to concole wlecome msg
+    </button>
+    <br />
+
+    <label for="modify">events modifiers(key modifiers)</label>
+    <input id="modify" type="text" @keyup.enter="handleInput" />
+    <p>{{ inputEventModifiers }}</p>
   </div>
 </template>
 
@@ -34,9 +24,47 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  data() {
+    return {
+      firstName: 'Mohammed',
+      secondName: 'hossam',
+      number1: 0,
+      number2: 0,
+      inputEventModifiers: '',
+    };
+  },
+  methods: {
+    add() {
+      console.log(typeof this.number1);
+      console.log(this.number1 + this.number2);
+    },
+    setStateNumber1(e) {
+      this.number1 = +e.target.value;
+    },
+    welcome(msg, e) {
+      console.log(msg);
+      console.log(e);
+    },
+    handleInput(e) {
+      this.inputEventModifiers = e.target.value;
+    },
+  },
+  computed: {
+    fullName() {
+      console.log('compted full name gets called');
+      return `${this.firstName} ${this.secondName.toUpperCase()}`;
+    },
+  },
+  watch: {
+    number1() {
+      setTimeout(() => {
+        console.log(this.number1);
+      }, 3000);
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
